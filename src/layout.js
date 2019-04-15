721a679aff0c12f55b5509fe2ccc1993afdd04a7
zhentian-wan <answer881215@gmail.com>
2019-04-15 23:13:43 +0300

    ab_test

--- src/layout.js
+++ src/layout.js
@@ -1,8 +1,31 @@
 /**
  * Created by Answer1215 on 9/19/2015.
  */
-export default (ngModule) => {
+function applicationLoad($window, $rootScope, $timeout) {
+    /*$timeout(() => {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = function() {
+            callFunctionFromScript();
+        }
+        script.text="window.ab_test=true";
+        head.appendChild(script);
+    }, 100);*/
 
+    $window.onload = () => {
+        $rootScope.$watch(() => {
+            return $window.ab_test;
+        }, (newVal) => {
+            if (!!newVal) {
+                console.log('ab_test is here', newVal)
+            }
+        });
+    }
+}
+
+export default (ngModule) => {
+    ngModule.run(applicationLoad);
     require('./header/index')(ngModule);
     require('./body/index')(ngModule);
 };
