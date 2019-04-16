143c211cef00b93e0a7bb907aa2169de79cc5dcd
zhentian-wan <answer881215@gmail.com>
2019-04-16 08:56:20 +0300

    up to 10 seconds

--- src/layout.js
+++ src/layout.js
@@ -1,27 +1,31 @@
 /**
  * Created by Answer1215 on 9/19/2015.
  */
-function applicationLoad($window, $rootScope, $timeout) {
-    /*$timeout(() => {
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.onload = function() {
-            callFunctionFromScript();
-        }
-        script.text="window.ab_test=true";
-        head.appendChild(script);
-    }, 100);*/
+function applicationLoad($interval, $window, $rootScope, $timeout) {
 
     $window.onload = () => {
+        const promise = $interval(() => $window.ab_test, 500, 20)
+
         $rootScope.$watch(() => {
             return $window.ab_test;
         }, (newVal) => {
-            if (!!newVal) {
+            if (newVal !== undefined) {
                 console.log('ab_test is here', newVal)
+                $interval.cancel(promise);
             }
-        });
+        }, true);
     }
+
+    setTimeout(() => {
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.text="window.ab_test=false;";
+        head.appendChild(script);
+    }, 200);
+
+
 }
 
 export default (ngModule) => {
