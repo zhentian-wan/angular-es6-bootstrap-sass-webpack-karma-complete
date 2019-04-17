4ad531ebdafd3608a75adc5463c81951b02740c7
zhentian-wan <answer881215@gmail.com>
2019-04-17 08:14:56 +0300

    show stuff

--- src/body/body.js
+++ src/body/body.js
@@ -1,22 +1,32 @@
 /**
  * Created by Answer1215 on 9/19/2015.
  */
-class bodyCtrl{
-    constructor(){
-
-    }
+class bodyCtrl {
+  /**ngInject */
+  constructor($rootScope, $window) {
+    $rootScope.$watch(
+      () => {
+        return $window.ab_test;
+      },
+      newVal => {
+        if (!!newVal) {
+          this.hasAb = true;
+        }
+      }
+    );
+  }
 }
 
-function expBody(){
-    return {
-        restrict: 'E',
-        scope: {},
-        controller: bodyCtrl,
-        controllerAs: 'body',
-        template: require('./body.html')
-    };
+function expBody() {
+  return {
+    restrict: "E",
+    scope: {},
+    controller: bodyCtrl,
+    controllerAs: "vm",
+    template: require("./body.html")
+  };
 }
 
-export default (ngModule) => {
-    ngModule.directive('expBody', expBody);
+export default ngModule => {
+  ngModule.directive("expBody", expBody);
 };
